cbc7dea0c8dce5486277b2d8eccc0a4cc11d915f
Ali <ali.zzeynali@gmail.com>
2021-06-11 22:07:21 +0430

    setup view binding fix navigation component

--- app/src/main/java/ir/alizeyn/products/presentation/MainActivity.kt
+++ app/src/main/java/ir/alizeyn/products/presentation/MainActivity.kt
@@ -1,12 +1,23 @@
 package ir.alizeyn.products.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import ir.alizeyn.products.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHost)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
--- app/src/main/java/ir/alizeyn/products/presentation/products/ProductsFragment.kt
+++ app/src/main/java/ir/alizeyn/products/presentation/products/ProductsFragment.kt
@@ -1,24 +1,33 @@
 package ir.alizeyn.products.presentation.products
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import ir.alizeyn.products.R
+import ir.alizeyn.products.databinding.FragmentProductsBinding
 
 
 class ProductsFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var _binding: FragmentProductsBinding? = null
+    private val binding: FragmentProductsBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_products, container, false)
+    ): View {
+        _binding = FragmentProductsBinding.inflate(inflater, container, false)
+        binding.root.setOnClickListener {
+            findNavController().navigate(R.id.action_productsFragment_to_detailFragment)
+        }
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
